3e857345bfbdf5e4f79b31534447bdc6be24f55e
Eric Willroth <ewillroth@gmail.com>
2020-12-25 08:04:55 -0600

    set up env

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from 'react';
+import firebase from 'firebase';
+
 const App = () => {
-	return <div>hi</div>;
+	return (
+		<div>
+			<p>hi</p>
+		</div>
+	);
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
+
+const config = {
+	apiKey: process.env.REACT_APP_FIREBASE_API,
+	authDomain: 'settle-io.firebaseapp.com',
+	databaseURL: 'https://settle-io.firebaseio.com',
+	projectId: 'settle-io',
+	storageBucket: 'gs://settle-io.appspot.com/',
+	messagingSenderId: '985342258862',
+};
+
+firebase.initializeApp(config);
+
+const storage = firebase.storage();
+const database = firebase.database();
+const auth = firebase.auth();
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -14,3 +30,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export { storage, database, auth };
